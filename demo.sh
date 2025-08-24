9966efb037766ccf67e0dfec33f9f6a83ce85408
denesegl <denesegl@student.21-school.ru>
2025-08-24 20:35:04 +0300

    removee files

--- demo.sh
+++
@@ -1,8 +0,0 @@
-curl -s http://localhost:8081/healthz | jq .
-
-curl -s http://localhost:8081/order/b563feb7b2b84b6test | jq .
-
-curl -s -o /dev/null -w "t=%{time_total}\n" http://localhost:8081/order/b563feb7b2b84b6test
-
-b563feb7b2b84b6test
-./scripts/produce.sh fixtures/model.json DIFF_KEY
